e68c6bd8064ba8ad38e50b38d77fbfcf8702988e
pritzest <joshuapritz.estrada.iics@ust.edu.ph>
2022-04-20 21:22:23 +0800

    Fixed frontend design

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,29 +3,31 @@ import { Link } from 'react-router-dom';
 import { Alert } from 'react-alert';
 
 function LoginForm() {
-    
-    return (
-        <div>
-            
-        <form action="/homepage">
-            <div className="form-inner">
-                <h2>Login</h2>
-                <div className='form-group'>
-                    <label htmlFor='name'>Student ID No:</label>
-                    <input type="text" name="idno" id="idno"required></input>
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='password'>Password:</label>
-                    <input type="password" name="password" id="password" required></input>
-                </div>
-                <input type="submit" value="SUBMIT" />
-                <input type="reset" value="CANCEL" />
 
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <div className='w-full max-w-xs'>
+                <form action="/homepage" className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+                    <div className="form-inner">
+                        <h2 className='font-medium leading-tight text-3xl mt-0 mb-2 text-red-500'>Login</h2>
+                        <div className='form-group mb-4'>
+                            <label htmlFor='name' className='block text-gray-700 text-sm font-bold mb-2'>Student ID No:</label>
+                            <input type="text" name="idno" id="idno" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'></input>
+                        </div>
+                        <div className='form-group '>
+                            <label htmlFor='password' className='block text-gray-700 text-sm font-bold mb-2'>Password:</label>
+                            <input type="password" name="password" id="password" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'></input>
+                        </div>
+                        <input type="submit" value="SUBMIT" className='shadow bg-red-400 hover:bg-red-200 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'/>
+                        <input type="reset" value="CANCEL" className='shadow bg-red-800 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded ml-2' />
+
+                    </div>
+                </form>
+                <p><Link to="/register" className='block text-gray-700 text-sm font-bold mb-2 ' > Register here!</Link> </p>
             </div>
-        </form>
-        <p><Link to="/register"> Register here!</Link> </p>
         </div>
-        
+
+
     )
 }
 
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,62 +1,69 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Alert } from 'react-alert';
 
 import '../index.css'
 
 
 export default function Registration() {
- 
+    const navigate = useNavigate();
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert('The details are being saved...');
+        return navigate('/homepage')
+    }
     return (
-        <div className="text-center m-5-auto">
-            <h2>Student Portal Registration</h2>
-            <h5>Please input your details below</h5>
-            <form action="/homepage">
-                <p>
-                    <label>Stundent ID Number</label><br/>
-                    <input type="number" name="IDno" required />
-                </p>
-                <p>
-                    <label>Last Name</label><br/>
-                    <input type="text" name="lName" required />
-                </p>
-                <p>
-                    <label>Given Name</label><br/>
-                    <input type="text" name="gName" requiredc />
-                </p>
-                <p>
-                    <label>Middle Name</label><br/>
-                    <input type="text" name="mName" requiredc />
-                </p>
-                <p>
-                    <label>College</label><br/>
-                    <input type="text" name="college" requiredc />
-                </p>
-                <p>
-                    <label>Program Enrolled</label><br/>
-                    <input type="text" name="pEnroll" requiredc />
-                </p>
-                <p>
-                    <label>Year Level</label><br/>
-                    <input type="number" name="yLevel" requiredc />
-                </p>
-                <p>
-                    <label>Password</label><br/>
-                    <input id="pass" type="password" name="pass" requiredc />
-                </p>
-                <p>
-                    <label>Confim Password</label><br/>
-                    <input id="cpass" type="password" name="cpass" requiredc />
-                </p>                
-                <p>
-                <input id="reg_btn" type="submit" value="REGISTER"/>
-                    <input id="can_btn" type="reset" value="CANCEL" />
-                </p>
-            </form>
-            <footer>
-                <h3>Already have an account?</h3>
-                <p><Link to="/">Go to Login Page</Link></p>
-            </footer>
+        <div className="flex justify-center items-center h-screen m-20">
+            <div className='w-full max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-20'>
+                <h2 className="font-medium leading-tight text-3xl mt-0 mb-2 text-red-500">Student Portal Registration</h2>
+                <h5 className=" leading-tight text-1xl mt-0 mb-2 text-black">Please input your details below</h5>
+                <form className="" onSubmit={handleSubmit}>
+                    <p className="">
+                        <label className="block text-gray-700 text-sm font-bold mb-0">Student ID Number</label><br />
+                        <input type="number" name="IDno" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'/>
+                    </p>
+                    <p>
+                        <label className="block text-gray-700 text-sm font-bold mb-0" >Last Name</label><br />
+                        <input type="text" name="lName" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'/>
+                    </p>
+                    <p>
+                        <label className="block text-gray-700 text-sm font-bold mb-0" >Given Name</label><br />
+                        <input type="text" name="gName" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'/>
+                    </p>
+                    <p>
+                        <label className="block text-gray-700 text-sm font-bold mb-0" >Middle Name</label><br />
+                        <input type="text" name="mName" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'/>
+                    </p>
+                    <p>
+                        <label className="block text-gray-700 text-sm font-bold mb-0" >College</label><br />
+                        <input type="text" name="college" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'/>
+                    </p>
+                    <p>
+                        <label className="block text-gray-700 text-sm font-bold mb-0">Program Enrolled</label><br />
+                        <input type="text" name="pEnroll" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'/>
+                    </p>
+                    <p>
+                        <label className="block text-gray-700 text-sm font-bold mb-0">Year Level</label><br />
+                        <input type="number" name="yLevel" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'/>
+                    </p>
+                    <p>
+                        <label className="block text-gray-700 text-sm font-bold mb-0">Password</label><br />
+                        <input id="pass" type="password" name="pass" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'/>
+                    </p>
+                    <p>
+                        <label className="block text-gray-700 text-sm font-bold mb-0">Confim Password</label><br />
+                        <input id="cpass" type="password" name="cpass" required className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'/>
+                    </p>
+                    <p>
+                        <input id="reg_btn" type="submit" value="REGISTER" className='shadow bg-red-400 hover:bg-red-200 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'/>
+                        <input id="can_btn" type="reset" value="CANCEL" className='shadow bg-red-800 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded ml-2'/>
+                    </p>
+                </form>
+                <footer>
+                    <h3 className="block text-gray-700 text-sm font-bold mb-2">Already have an account?</h3>
+                    <p className="block text-gray-700 text-sm font-bold mb-2"><Link to="/">Go to Login Page</Link></p>
+                </footer>
+            </div>
         </div>
     )
 
